fa575f0b528c3e324ad0f390025a5d3c361c0779
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-06-14 12:26:27 +0300

    Create singletone.cpp

--- singletone.cpp
+++ singletone.cpp
@@ -1,3 +1,27 @@
+
+// Singleton.h
+class Singleton
+{
+  private:
+    static Singleton * p_instance;
+    Singleton() {
+    Singleton( const Singleton& );  
+    Singleton& operator=( Singleton& );
+  public:
+    static Singleton * getInstance() {
+        if(!p_instance)           
+            p_instance = new Singleton();
+        return p_instance;
+    }
+};
+
+   **************************************
+
+// Singleton.cpp
+#include "Singleton.h"
+  
+Singleton* Singleton::p_instance = 0;
+
 // Singleton.h
 class Singleton
 {
