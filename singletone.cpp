1b293110bbf6a37c139c9a577fdf06a89c782aec
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-06-14 10:56:26 +0300

    Create singletone.cpp

---
+++ singletone.cpp
@@ -0,0 +1,13 @@
+// Singleton.h
+class Singleton
+{
+private: 
+    Singleton() {}
+    Singleton( const Singleton&);  
+    Singleton& operator=( Singleton& );
+public:
+    static Singleton& getInstance() {
+        static Singleton  instance;
+        return instance;
+    }    
+}; 
